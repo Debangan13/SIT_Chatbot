874b1140f73e4b6bcbadbb9e43238b39103652ca
debangan13 <debangan.barman@gmail.com>
2023-02-28 10:18:50 -0800

    first commit

---
+++ ex.py
@@ -0,0 +1,25 @@
+import openai
+openai.api_key = "Your-API-Key" # or use the method we defined earlier
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
+
+    
+while True:
+    chat = input("You : ")
+    if chat == "bye" or chat == "bye" or chat == "Exit" or chat == "goodbye":
+        print("Ok..\nThank\'s.. ")
+        break
+    
+    response = generate_response(chat)
+    print("Bot: ",response)
+        
